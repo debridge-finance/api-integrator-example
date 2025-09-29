7468c8efec1a32763b71a480623e370f28c4c6b1
Damir Bajramovic <bajramovic.damir@hotmail.com>
2025-09-29 15:25:27 +0200

    Add example hash.

--- src/scripts/same-chain/queries/by-transaction-id.ts
+++ src/scripts/same-chain/queries/by-transaction-id.ts
@@ -2,7 +2,7 @@ import { get } from "../../../utils/http";
 
 async function main() {
   const chainId = 137; // Example chain ID (Polygon)
-  const transactionHash = '<setTransactionHash>'; // Transaction hash
+  const transactionHash = '0xdefcfc8ee63ce48a8b19a34f289d6cf90d6b722b2d8df74083aa197c6c43d537'; // Transaction hash
   const URL = `https://stats-api.dln.trade/api/SameChainSwap/${chainId}/tx/${transactionHash}`;
 
   const data = await get(URL);
